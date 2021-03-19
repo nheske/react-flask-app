21db65d1843aadd08e65b89372684dc8b99fc901
nheske <normheske@gmail.com>
2021-03-19 11:49:57 -0700

    update readme

--- api/api.py
+++ api/api.py
@@ -1,8 +1,13 @@
 import time
-from flask import flask
+from flask import Flask
 
-app= flask(__name__)
+app= Flask(__name__)
 
 @app.route('/time')
 def get_current_time():
+    # return {'time': "hello"}
+    ticks = time.time()
+    print("Number of ticks since 12:00am, January 1, 1970:", ticks)
     return {'time': time.time()}
+
+print(get_current_time())
